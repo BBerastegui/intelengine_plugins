60ad3d8766a8638c32424a9b3c8c1f7e178f512e
BBerastegui <BBerastegui@users.noreply.github.com>
2015-01-31 20:04:12 +0100

    WIP...

--- dork.go
+++ dork.go
@@ -9,8 +9,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	//	"log"
-	//	"net/http"
+	"log"
+	"net/http"
 )
 
 // Received: {"url":"github.com","filetype":["pdf","doc"]}
@@ -18,7 +18,6 @@ type Dork struct{
 	Url string
 	Filetype []string
 }
-
 
 
 func main() {
@@ -34,4 +33,42 @@ func main() {
 		log.Println("[/!\\]Error in unmarshal:", err)
 	}
 	fmt.Println(dork)
+
+	performRequest(dork)
 }
+
+func performRequest(dork Dork){
+	// Handle number of files
+	var filetypes string
+	if (len(dork.Filetype)>1){
+		filetypes = dork.Filetype[0]
+	} else {
+		filetypes = dork.Filetype[0]
+	}
+	// Set the full url
+	url := "http://www.google.es/search?start=100&num=100&filter=0&q=site:"+dork.Url+"+filetype:"+filetypes+"&gws_rd=cr"
+
+	// Setup the request to the target
+	req, err := http.NewRequest("GET", url, nil)
+	// Add the custom headers
+	req.Header.Set("User-Agent", "ChromeCaffeine 01.00")
+
+	client := &http.Client{}
+	// Perform request and store response on "resp"
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("[/!\\]Error in response:", err)
+	}   
+	defer resp.Body.Close()
+
+	// Handle response (resp)
+
+	// Store HTTP Status code
+	fmt.Println(resp.StatusCode)
+	// Store Headers
+	fmt.Println(resp.Header)
+	// Store Body
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
+
